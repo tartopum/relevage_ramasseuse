a65804ffaeb6d76bb5a0212c5dd5e864cd6f5bbf
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-11-28 12:17:20 +0100

    Set the pin modes

--- arduino/Knob.cpp
+++ arduino/Knob.cpp
@@ -1,17 +1,22 @@
 #include "Knob.h"
 
 Knob::Knob(
-      int minInputVal,
-      int maxInputVal,
-      byte pin
+  int minInputVal,
+  int maxInputVal,
+  byte pin
 ) {
   _minInputVal = minInputVal;
   _maxInputVal = maxInputVal;
   _pin = pin;
+
+  pinMode(_pin, INPUT);
 }
 
 float readTargetPosRatio() {
   const int inputVal = analogRead(_pin);
+  if (inputVal < _minInputVal) {
+    return 0;
+  }
   if (inputVal > _maxInputVal) {
     return 1;
   }
